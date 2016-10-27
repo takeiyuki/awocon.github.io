5c59d4c706f34ee6ee648326adcfc7cb3a9b08c3
Takei Yuki <yuki-takei@altplus.co.jp>
2016-10-27 20:40:41 +0900

    タブを修正

--- leetcode/q1/test.c
+++ leetcode/q1/test.c
@@ -5,28 +5,28 @@
 #include<stdlib.h>
 int result[2];
 int* twoSum(int* nums, int numsSize, int target) {
-    int sumResult;
-    int *numsCol;
-    int *numsRow;
-    
-    for(numsRow = nums; (numsRow - nums) < numsSize; numsRow++) {
-        for(numsCol = (numsRow + 1);(numsCol - nums) < numsSize; numsCol++) {
-            if (*numsRow + *numsCol == target) {
-                result[0] = (numsRow-nums);
-                result[1] = (numsCol-nums);
-                return result;
-            }
-        }
-    }
-    return result;
+	int sumResult;
+	int *numsCol;
+	int *numsRow;
+	
+	for(numsRow = nums; (numsRow - nums) < numsSize; numsRow++) {
+		for(numsCol = (numsRow + 1);(numsCol - nums) < numsSize; numsCol++) {
+			if (*numsRow + *numsCol == target) {
+				result[0] = (numsRow-nums);
+				result[1] = (numsCol-nums);
+				return result;
+			}
+		}
+	}
+	return result;
 }
 
 int main() {
-    int* nums = (int*)malloc(sizeof(int)*3);
- 	nums[0] = 3;
+	int* nums = (int*)malloc(sizeof(int)*3);
+	nums[0] = 3;
 	nums[1] = 2;
 	nums[2] = 4;
-    int* test = (int*)malloc(sizeof(int)*2);
+	int* test = (int*)malloc(sizeof(int)*2);
 	test = twoSum(nums, 3, 6);
 	printf("%d,%d",test[0],test[1]);
 }
